12bc65b223c7917d387e7bc016e442deb5a8f2df
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-05 17:12:46 -0500

    start playing with the docx files

--- templateextractor.py
+++ templateextractor.py
@@ -1,5 +1,5 @@
-# from docx import Document
-# from docx.shared import Inches
+from docx import Document
+from docx.shared import Inches
 import olefile
 import argparse
 
@@ -10,27 +10,37 @@ args = parser.parse_args()
 
 
 
-# document = Document(args.filename)
+document = Document(args.filename)
+print(document)
 
-print(olefile.isOleFile(args.filename))
-with  olefile.OleFileIO(args.filename) as ole:
-	print(ole.listdir())
-	if ole.exists('worddocument'):
-		print("This is a Word document.")
-		doc = ole.openstream('worddocument')
-		data = doc.read()
-		# print(data)
-		print('1Table')
-		doc = ole.openstream('1Table')
-		doc.listdir()
-		data = doc.read()
-		print(data)
-		print('Data')
-		doc = ole.openstream('Data')
-		data = doc.read()
-		print(data)
+
+sections = document.sections
+for section in sections:
+	print(section.start_type)
+	print(section.page_height, section.page_width)
+	# section.orientation
+	print(section.page_height.inches, section.page_width.inches)
+
+
+# print(olefile.isOleFile(args.filename))
+# with  olefile.OleFileIO(args.filename) as ole:
+# 	print(ole.listdir())
+# 	if ole.exists('worddocument'):
+# 		print("This is a Word document.")
+# 		doc = ole.openstream('worddocument')
+# 		data = doc.read()
+# 		# print(data)
+# 		print('1Table')
+# 		doc = ole.openstream('1Table')
+# 		doc.listdir()
+# 		data = doc.read()
+# 		print(data)
+# 		print('Data')
+# 		doc = ole.openstream('Data')
+# 		data = doc.read()
+# 		print(data)
 		
-		if ole.exists('macros/vba'):
-			print("This document seems to contain VBA macros.")
-	# meta = ole.get_metadata()
-	# meta.dump()
+# 		if ole.exists('macros/vba'):
+# 			print("This document seems to contain VBA macros.")
+# 	# meta = ole.get_metadata()
+# 	# meta.dump()
