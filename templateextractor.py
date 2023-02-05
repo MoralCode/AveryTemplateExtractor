981276188b94bd8e6f383acc1b2e51a822149cdd
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-05 17:01:51 -0500

    playing with .doc files

--- templateextractor.py
+++ templateextractor.py
@@ -1,6 +1,6 @@
-from docx import Document
-from docx.shared import Inches
-import argparse
+# from docx import Document
+# from docx.shared import Inches
+import olefile
 import argparse
 
 parser = argparse.ArgumentParser(description='extract template information from a word template')
@@ -10,4 +10,27 @@ args = parser.parse_args()
 
 
 
-document = Document(args.filename)
+# document = Document(args.filename)
+
+print(olefile.isOleFile(args.filename))
+with  olefile.OleFileIO(args.filename) as ole:
+	print(ole.listdir())
+	if ole.exists('worddocument'):
+		print("This is a Word document.")
+		doc = ole.openstream('worddocument')
+		data = doc.read()
+		# print(data)
+		print('1Table')
+		doc = ole.openstream('1Table')
+		doc.listdir()
+		data = doc.read()
+		print(data)
+		print('Data')
+		doc = ole.openstream('Data')
+		data = doc.read()
+		print(data)
+		
+		if ole.exists('macros/vba'):
+			print("This document seems to contain VBA macros.")
+	# meta = ole.get_metadata()
+	# meta.dump()
